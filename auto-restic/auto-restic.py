430554ace800ba61e23d9af3ceeb8734ab07d5ca
499602D2 <46869890+499602D2@users.noreply.github.com>
2021-08-20 16:11:59 +0300

    Handle exceptions when dumping config json
    This should avoid loss of the configuration file due to a failed write.

--- auto-restic/auto-restic.py
+++ auto-restic/auto-restic.py
@@ -1,6 +1,6 @@
-'''
+"""
 Automated and scheduled restic backups with configuration files.
-'''
+"""
 
 import os
 import sys
@@ -20,99 +20,99 @@ SCHEDULER = BackgroundScheduler()
 SCHEDULER.start()
 
 def run_backup(config: dict, config_path: str):
-	'''
+	"""
 	Run the backup.
-	'''
+	"""
 	home = str(Path.home())
 	bfile = Path(os.path.join(BASE_DIR, config["backup-file"]))
 	efile = Path(os.path.join(BASE_DIR, config["exclude-file"]))
-	pfile = Path(os.path.join(BASE_DIR, config['restic-password-file']))
+	pfile = Path(os.path.join(BASE_DIR, config["restic-password-file"]))
 
 	# escape spaces in repo path
-	repo = Path(config['restic-repo'].replace(" ", "\ "))
+	repo = Path(config["restic-repo"].replace(" ", "\ "))
 
-	cmd = f'restic -r {repo} backup --files-from {bfile} --verbose --password-file {pfile}'
-	if config['exclude-file'] is not None:
-		cmd += f' --exclude-file={efile}'
+	cmd = f"restic -r {repo} backup --files-from {bfile} --verbose --password-file {pfile}"
+	if config["exclude-file"] is not None:
+		cmd += f" --exclude-file={efile}"
 
 	# run command and clean repository
 	os.system(cmd)
 	clean_repository(config)
 
 	# update config with update time
-	config['last-backed-up'] = int(time.time())
+	config["last-backed-up"] = int(time.time())
 	update_config(config, config_path)
 
 	# schedule next backup
-	backup_every_sec = int(config['backup-frequency'])
+	backup_every_sec = int(config["backup-frequency"])
 
 	backup_dt = datetime.datetime.fromtimestamp(int(time.time()) + backup_every_sec)
 	SCHEDULER.add_job(
 		run_backup,
-		'date', run_date=backup_dt,
-		args=[load_config(config_path=CONFIG_DIR), CONFIG_DIR],
-		id=f'backup-{int(time.time()) + backup_every_sec}'
+		"date", run_date=backup_dt,
+		args=[ load_config(config_path=CONFIG_DIR), CONFIG_DIR ],
+		id=f"backup-{int(time.time()) + backup_every_sec}"
 	)
 
-	logging.info(f'Backup completed! Next backup scheduled for {backup_dt}')
+	logging.info(f"Backup completed! Next backup scheduled for {backup_dt}")
 	print("\n✅ Backup completed! You can now exit the program.")
 
 
 def clean_repository(config: dict):
-	'''
+	"""
 	Prune restic repository after backup.
-	'''
-	repo = config['restic-repo']
-	pfile = os.path.join(BASE_DIR, config['restic-password-file'])
-	keep_backups_for = config['keep-backups']
+	"""
+	repo = config["restic-repo"]
+	pfile = os.path.join(BASE_DIR, config["restic-password-file"])
+	keep_backups_for = config["keep-backups"]
 
-	if keep_backups_for not in ('-1', -1):
-		cmd = f'restic -r {repo} forget --keep-within {keep_backups_for} --prune --password-file {pfile}'
+	if keep_backups_for not in ("-1", -1):
+		cmd = f"restic -r {repo} forget --keep-within {keep_backups_for} --prune --password-file {pfile}"
 	else:
-		logging.info('Backups configured to be kept forever: not removing or pruning.')
+		logging.info("Backups configured to be kept forever: not removing or pruning.")
 		return
 
-	os.system(cmd + ' --dry-run')
+	os.system(cmd + " --dry-run")
 
 
 def apscheduler_event_listener(event):
-	'''
-	Listens to exceptions coming in from apscheduler's threads.
-	'''
+	"""
+	Listens to exceptions coming in from apscheduler"s threads.
+	"""
 	if event.exception:
-		logging.critical(f'Error: scheduled job raised an exception: {event.exception}')
-		logging.critical('Exception traceback follows:')
+		logging.critical(f"Error: scheduled job raised an exception: {event.exception}")
+		logging.critical("Exception traceback follows:")
 		logging.critical(event.traceback)
 
 
-if __name__ == '__main__':
-	CONFIG_DIR = os.path.join(os.path.dirname(__file__), "configuration" , 'restic-config.json')
+if __name__ == "__main__":
+	CONFIG_DIR = os.path.join(os.path.dirname(__file__), "configuration" , "restic-config.json")
 	SCHEDULER.add_listener(apscheduler_event_listener, EVENT_JOB_ERROR)
 
 	# log path
 	log = os.path.join(BASE_DIR, "logs", "backup.log")
 	if not os.path.isdir(os.path.join(BASE_DIR, "logs")):
-		os.makedirs(OS.PATH.JOIN(BASE_DIR, "logs"))
+		os.makedirs(os.path.join(BASE_DIR, "logs"))
 
 	# init log (disk)
-	logging.getLogger('apscheduler').setLevel(logging.WARNING)
+	logging.getLogger("apscheduler").setLevel(logging.WARNING)
 	logging.basicConfig(
 		filename=log, level=logging.DEBUG,
-		format='%(asctime)s %(message)s', datefmt='%d/%m/%Y %H:%M:%S'
+		format="%(asctime)s %(message)s", datefmt="%d/%m/%Y %H:%M:%S"
 	)
 
 	conf = load_config(CONFIG_DIR)
-	last_backup = conf['last-backed-up']
+	last_backup = conf["last-backed-up"]
 
-	if last_backup is None or conf['backup-on-start']:
-		if conf['backup-on-start']:
-			logging.info('Backup configured to run on program start: running now...')
+	if last_backup is None or conf["backup-on-start"]:
+		if conf["backup-on-start"]:
+			logging.info("Backup configured to run on program start: running now...")
 		else:
-			logging.info('Backup has never been run: running now...')
+			logging.info("Backup has never been run: running now...")
 
 		run_backup(conf, CONFIG_DIR)
 	else:
-		next_backup_unix_ts = int(last_backup) + int(conf['backup-frequency'])
+		next_backup_unix_ts = int(last_backup) + int(conf["backup-frequency"])
 		next_backup_dt = datetime.datetime.fromtimestamp(next_backup_unix_ts)
 
 		if next_backup_unix_ts <= int(time.time()):
@@ -120,13 +120,13 @@ if __name__ == '__main__':
 		else:
 			SCHEDULER.add_job(
 				run_backup,
-				'date', run_date=next_backup_dt,
-				args=[load_config(config_path=CONFIG_DIR), CONFIG_DIR], id=f'backup-{next_backup_unix_ts}')
+				"date", run_date=next_backup_dt,
+				args=[load_config(config_path=CONFIG_DIR), CONFIG_DIR], id=f"backup-{next_backup_unix_ts}")
 
-			logging.info(f'Next backup scheduled for {next_backup_dt}')
+			logging.info(f"Next backup scheduled for {next_backup_dt}")
 
 	while True:
 		try:
 			time.sleep(3600)
 		except KeyboardInterrupt:
-			sys.exit('Got ctrl+c: exiting...')
+			sys.exit("Got ctrl+c: exiting...")
--- auto-restic/config.py
+++ auto-restic/config.py
@@ -6,90 +6,102 @@ import ujson as json
 BASE_DIR = os.path.dirname(__file__)
 
 def create_config():
-	'''
+	"""
 	Create a new configuration and associated files.
-	'''
+	"""
 
 	# repository path
-	repo_path = input('restic repository path: ')
+	repo_path = input("restic repository path: ")
 
 	# ask for password file path and password
-	pw_path = input('restic password file path (configuration/restic-password.txt): ')
+	pw_path = input("restic password file path (configuration/restic-password.txt): ")
 	if pw_path == "":
 		pw_path = os.path.join(BASE_DIR, "configuration", "restic-password.txt")
 
-	with open(pw_path, 'w') as pw_file:
-		pw_file.write(input('Enter restic repository password: '))
+	with open(pw_path, "w") as pw_file:
+		pw_file.write(input("Enter restic repository password: "))
 
 	# backup frequency
-	backup_freq = int(eval(input('Backup every .. seconds (e.g. 3600*24): ')))
+	backup_freq = int(eval(input("Backup every .. seconds (e.g. 3600*24): ")))
 	if backup_freq == "":
 		backup_freq = 3600*24
 
 	# keep backups
-	keep_backups = input('Time to keep backups (e.g. 2y5m7d3h, -1=forever): ')
+	keep_backups = input("Time to keep backups (e.g. 2y5m7d3h, -1=forever): ")
 	if keep_backups == "":
 		keep_backups = "-1"
 
 	paths_to_backup = []
-	inp = input('\nAdd paths/files to BACKUP (enter when done): ')
-	while inp != '':
+	inp = input("\nAdd paths/files to BACKUP (enter when done): ")
+	while inp != "":
 		paths_to_backup.append(inp)
-		inp = input('Add paths/files to BACKUP (enter when done): ')
+		inp = input("Add paths/files to BACKUP (enter when done): ")
 
 	paths_to_exclude = []
-	inp = input('\nAdd paths/files to EXCLUDE (enter when done): ')
-	while inp != '':
+	inp = input("\nAdd paths/files to EXCLUDE (enter when done): ")
+	while inp != "":
 		paths_to_exclude.append(inp)
-		inp = input('Add paths/files to EXCLUDE (enter when done): ')
+		inp = input("Add paths/files to EXCLUDE (enter when done): ")
 
 	# write paths to file
 	backup_paths_fname = os.path.join(BASE_DIR, "configuration", "paths-to-backup.txt")
-	with open(backup_paths_fname, 'w') as backup_file:
+	with open(backup_paths_fname, "w") as backup_file:
 		backup_file.write("\n".join(paths_to_backup))
 
 	# create exclude file
 	exclude_paths_fname = os.path.join(BASE_DIR, "configuration", "paths-to-exclude.txt")
-	with open(exclude_paths_fname, 'w') as exclude_file:
+	with open(exclude_paths_fname, "w") as exclude_file:
 		exclude_file.write("\n".join(paths_to_exclude))
 
 	# backup on startup?
 	inp = input("\nAlways backup on script startup? (y/N): ")
-	backup_on_start = bool(inp.lower() in ('y', 'yes'))
+	backup_on_start = bool(inp.lower() in ("y", "yes"))
 
 	# creat config
 	config = {
-		'restic-repo': repo_path,
-		'restic-password-file': pw_path,
-		'backup-file': backup_paths_fname,
-		'exclude-file': exclude_paths_fname,
-		'backup-frequency': backup_freq,
-		'keep-backups': keep_backups,
-		'backup-on-start': backup_on_start,
-		'last-backed-up': None
+		"restic-repo": repo_path,
+		"restic-password-file": pw_path,
+		"backup-file": backup_paths_fname,
+		"exclude-file": exclude_paths_fname,
+		"backup-frequency": backup_freq,
+		"keep-backups": keep_backups,
+		"backup-on-start": backup_on_start,
+		"last-backed-up": None
 	}
 
 	return config
 
 
 def update_config(new_config: dict, config_path: str):
-	'''
+	"""
 	Dump updated config to disk.
-	'''
-	with open(config_path, 'w') as config_file:
-		json.dump(new_config, config_file, indent=4)
+	"""
+	# tmp file path
+	tmp_file_path = "/".join(config_path.split("/")[:-1] + ["tmp.json"])
+
+	# open tmp file
+	try:
+		with open(tmp_file_path, "w") as tmp_file:
+			json.dump(new_config, tmp_file, indent=4)
+	except:
+		print("⚠️ Error dumping updated config!")
+		os.remove(tmp_file_path)
+		return
+
+	# if successful, rename temp file
+	os.rename(tmp_file_path, config_path)
 
 
 def load_config(config_path: str):
-	'''
+	"""
 	Load config from path.
-	'''
+	"""
 	if not os.path.isfile(config_path):
-		with open(config_path, 'w') as config_file:
+		with open(config_path, "w") as config_file:
 			json.dump(create_config(), config_file, indent=4)
 
 	try:
-		with open(config_path, 'r') as config_file:
+		with open(config_path, "r") as config_file:
 			return json.load(config_file)
 	except ValueError:
 		sys.exit("Error loading configuration file!")
