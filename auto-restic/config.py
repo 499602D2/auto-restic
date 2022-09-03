9c8a9fabaae490105104891668b6b976668354b7
499602D2 <46869890+499602D2@users.noreply.github.com>
2022-09-03 14:14:26 +0300

    Update config.py
    * Lint
    * Use pathlib
    * Use logging
    * Handle possible exceptions when dumping config

--- auto-restic/config.py
+++ auto-restic/config.py
@@ -1,9 +1,11 @@
+import logging
 import os
 import sys
 
 import ujson as json
 
 BASE_DIR = os.path.dirname(__file__)
+
 
 def create_config():
 	"""
@@ -14,9 +16,11 @@ def create_config():
 	repo_path = input("restic repository path: ")
 
 	# ask for password file path and password
-	pw_path = input("restic password file path (configuration/restic-password.txt): ")
+	pw_path = input(
+		"restic password file path (configuration/restic-password.txt): ")
 	if pw_path == "":
-		pw_path = os.path.join(BASE_DIR, "configuration", "restic-password.txt")
+		pw_path = os.path.join(BASE_DIR, "configuration",
+			"restic-password.txt")
 
 	with open(pw_path, "w") as pw_file:
 		pw_file.write(input("Enter restic repository password: "))
@@ -24,7 +28,7 @@ def create_config():
 	# backup frequency
 	backup_freq = int(eval(input("Backup every .. seconds (e.g. 3600*24): ")))
 	if backup_freq == "":
-		backup_freq = 3600*24
+		backup_freq = 3600 * 24
 
 	# keep backups
 	keep_backups = input("Time to keep backups (e.g. 2y5m7d3h, -1=forever): ")
@@ -44,12 +48,14 @@ def create_config():
 		inp = input("Add paths/files to EXCLUDE (enter when done): ")
 
 	# write paths to file
-	backup_paths_fname = os.path.join(BASE_DIR, "configuration", "paths-to-backup.txt")
+	backup_paths_fname = os.path.join(BASE_DIR, "configuration",
+		"paths-to-backup.txt")
 	with open(backup_paths_fname, "w") as backup_file:
 		backup_file.write("\n".join(paths_to_backup))
 
 	# create exclude file
-	exclude_paths_fname = os.path.join(BASE_DIR, "configuration", "paths-to-exclude.txt")
+	exclude_paths_fname = os.path.join(BASE_DIR, "configuration",
+		"paths-to-exclude.txt")
 	with open(exclude_paths_fname, "w") as exclude_file:
 		exclude_file.write("\n".join(paths_to_exclude))
 
@@ -72,19 +78,19 @@ def create_config():
 	return config
 
 
-def update_config(new_config: dict, config_path: str):
+def update_config(new_config: dict, config_path):
 	"""
 	Dump updated config to disk.
 	"""
 	# tmp file path
-	tmp_file_path = "/".join(config_path.split("/")[:-1] + ["tmp.json"])
+	tmp_file_path = config_path.parent / "tmp.json"
 
 	# open tmp file
 	try:
 		with open(tmp_file_path, "w") as tmp_file:
 			json.dump(new_config, tmp_file, indent=4)
 	except:
-		print("⚠️ Error dumping updated config!")
+		logging.exception("⚠️ Error dumping updated config!")
 		os.remove(tmp_file_path)
 		return
 
@@ -98,7 +104,12 @@ def load_config(config_path: str):
 	"""
 	if not os.path.isfile(config_path):
 		with open(config_path, "w") as config_file:
-			json.dump(create_config(), config_file, indent=4)
+			try:
+				json.dump(create_config(), config_file, indent=4)
+			except:
+				logging.exception(
+					"Loading config failed in load_config (file did not exist)"
+				)
 
 	try:
 		with open(config_path, "r") as config_file:
